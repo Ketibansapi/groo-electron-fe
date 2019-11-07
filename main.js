00b442e66c0208696d12404c1cbc9a3c172aa819
Deary <dearyherdiano@gmail.com>
2019-11-07 12:51:19 +0800

    add reload ad main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
+require('electron-reload')(__dirname);
+
 const path = require('path')
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
